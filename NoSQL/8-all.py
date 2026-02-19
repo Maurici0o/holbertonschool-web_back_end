cee01e4ab2396ad6bb8834f5d809989b9d69d175
Maurici0o <mauricio_lemus@anahuac.mx>
2026-02-19 09:23:50 -0600

    .

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -2,6 +2,7 @@
 """Module to list all documents in a MongoDB collection"""
 
 def list_all(mongo_collection):
+    """Lists all documents in a collection"""
     if mongo_collection is None:
         return []
 
